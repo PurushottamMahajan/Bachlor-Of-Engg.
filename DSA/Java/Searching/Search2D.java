464d2187c2cecb455ab1aff95870c45b5be72288
Purushottam Mahajan <142152858+PurushottamMahajan@users.noreply.github.com>
2025-03-28 18:45:47 +0530

    Add files via upload

---
+++ DSA/Java/Searching/Search2D.java
@@ -0,0 +1,31 @@
+package codes;
+import java.util.*;
+public class Search2D
+{
+	public static void main(String a[])
+	{
+		//Array  is sorted row and colmn wise
+		int arr[][]={
+			{25,30,35},{28,31,37},{38,39,40}
+			};
+
+			System.out.print(Arrays.toString(search(arr,37)));
+	}
+	 static int[] search(int arr[][],int target)
+	{
+		int r=0;
+		int c=arr.length-1;
+
+		while(r<arr.length   &&  c>=0)
+		{
+			if(arr[r][c]==target)
+				return new int[]{r,c};
+			else if (arr[r][c]<target)
+				r++;
+			else if (arr[r][c]>target)
+				c--;
+		}
+		return new  int[]{-1,-1};
+	}
+
+}
